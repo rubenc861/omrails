5c94e3e4e01a51c7c676a282e2b191712d1b951f
Ruben Castillo <rubenc861@gmail.com>
2017-06-25 13:55:11 -0500

    Added about and contact, updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
  root 'pages#home'
-
+ #added about page 6/25/17
+ get 'about' => 'pages#about'
+ get 'contact-us' => 'pages#contact-us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
