7593e90d2255eba5a7d7ead95e03011bee01bff9
Ruben Castillo <rubenc861@gmail.com>
2017-07-12 19:21:50 -0500

    Add tweet and notice validations

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,3 +1,6 @@
 class Notice < ApplicationRecord
   belongs_to :user
+
+  validates :content, presence: true, length: {maximum: 1000}
+  validates :user, presence: true
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+
+  validates :content, presence: true, length: {maximum: 40}
+  validates :user, presence: true
 end
